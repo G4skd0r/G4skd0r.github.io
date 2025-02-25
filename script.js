b286bfbb5cda9396c588242701685e23c1c5c520
G4skd0r <64443958+G4skd0r@users.noreply.github.com>
2025-02-25 10:40:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function nextStep(formId) {
-    document.querySelector(`#${formId} #step-1`).style.display = "none";
-    document.querySelector(`#${formId} #step-2`).style.display = "block";
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".next-button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            const parentForm = this.closest("form");
+            parentForm.querySelector("#step-1").style.display = "none";
+            parentForm.querySelector("#step-2").style.display = "block";
+        });
+    });
+});
